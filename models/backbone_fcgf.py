8fc0514f71f2f2ba5d5945bc696b577566b9f581
aiday.kyzy <aiday.kyzy@astrivis.com>
2022-12-01 11:45:36 +0100

    phase here is encode

--- models/backbone_fcgf.py
+++ models/backbone_fcgf.py
@@ -8,7 +8,7 @@ class FCGF(nn.Module):
     def __init__(self, config):
         super(FCGF, self).__init__()
 
-    def forward(self, batch):
+    def forward(self, batch, phase = 'encode'):
         
         x = batch['features'].clone().detach()
         return torch.tensor(x).to('cuda:0')
