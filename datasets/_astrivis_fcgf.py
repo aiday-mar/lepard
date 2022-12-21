ed77936e8e1b504c789b0b13d90272f993045083
aiday.kyzy <aiday.kyzy@astrivis.com>
2022-12-21 11:37:26 +0100

    removing print statements inside of the python file

--- datasets/_astrivis_fcgf.py
+++ datasets/_astrivis_fcgf.py
@@ -123,10 +123,7 @@ class _AstrivisFCGF(Dataset):
         folder_number = index // self.n_files_per_folder
         idx_inside_folder = index % self.n_files_per_folder
         folder_string = 'model' + str(self.folders[folder_number]).zfill(3)
-        print('folder_string : ', folder_string)
         files_array = self.matches[folder_string]
-        print('files_array : ', files_array)
-        print('idx_inside_folder : ', idx_inside_folder)
         filename = files_array[idx_inside_folder]
         file_pointers = filename[:-4]
         file_pointers = file_pointers.split('_')
